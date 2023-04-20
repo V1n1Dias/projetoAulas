8b3bae4966ac057f22d8d7cac8d18646f5be22c2
s.lucas <havisvds3@gmail.com>
2023-04-20 15:03:05 -0300

    adicionando projeto netbeans

---
+++ src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,13 @@
+
+package javaapplication6;
+
+
+public class JavaApplication6 {
+
+   
+    public static void main(String[] args) {
+          
+        System.out.println(" Hello world!");
+        
+}
+}
